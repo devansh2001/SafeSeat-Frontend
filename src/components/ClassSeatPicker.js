940ae812af2e2358dc63d271002335ee2af08147
devansh2001 <dponda3@gatech.edu>
2021-01-22 18:57:32 -0500

    Added seat picker

---
+++ src/components/ClassSeatPicker.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SeatPicker from 'react-seat-picker'
+class ClassSeatPicker extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        <div>
+            Hello World
+        </div>
+    }
+}
+
+export default ClassSeatPicker;
