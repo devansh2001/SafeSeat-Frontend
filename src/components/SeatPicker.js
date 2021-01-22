da4150082b15c4bc016281fcbf142c7979e270c3
devansh2001 <dponda3@gatech.edu>
2021-01-22 18:51:48 -0500

    Added some components

---
+++ src/components/SeatPicker.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SeatPicker extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        
+    }
+}
+
+export default SeatPicker;
