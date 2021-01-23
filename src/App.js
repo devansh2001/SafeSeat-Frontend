2158c71e1b7eb62b8310d4ef02bce1b093ceec22
devansh2001 <dponda3@gatech.edu>
2021-01-23 13:12:56 -0500

    added signin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import ClassSeatPicker from './components/ClassSeatPicker';
+import Login from './components/Login';
+
 function App() {
   return (
     <div className="App">
-      <ClassSeatPicker />
+      <Login />
     </div>
   );
 }
