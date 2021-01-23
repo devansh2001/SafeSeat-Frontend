406b98f6e04ca0726c0cd4eb2cd0d3e6b975b4c8
Yasmine Soubai <yasminesoubai99@gmail.com>
2021-01-23 15:43:31 -0500

    Main page created

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import logo from './logo.png';
+
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div class = "main" className="mainPage">
+                <Button variant="" type="submit" className="signInBtn">
+                    Sign In
+                </Button>
+                <img src={logo} alt="Logo" className ="logoImage" />
+                <p className="ourMission">It’s time to go back safely to our campus and be able 
+                    to comfortably attend in person lectures. <br></br>We can 
+                    return to normalcy during a pandemic.
+                </p>
+                <Button variant="" type="submit" className="button">
+                    I am a Student
+                </Button>
+                <Button variant="" type="submit" className="button">
+                    I am a Professor
+                </Button>
+
+            </div>
+        );
+    }
+}
+
+export default Main;
